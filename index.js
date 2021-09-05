40cbeb899abe9bc0cbe87711ed501f654f32353f
Avishek-kr <ak251997@gmail.com>
2021-09-06 01:45:05 +0530

    development environment

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const express= require('express');
-
 const app=express();
+const port=8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
 
 app.get('/', (req,res)=>{
     // res.send("Hello World!");
@@ -15,7 +20,6 @@ app.get('/awesome',(req, res)=>{
     const {name, isAwesome} = req.query;
     res.send(`${name} is ${JSON.parse(isAwesome)? 'really': 'not really'} awesome`)
 });
-const port=8888;
 
 app.listen(port, ()=>{
     console.log(`Express app listening at http://localhost:${port}`);
